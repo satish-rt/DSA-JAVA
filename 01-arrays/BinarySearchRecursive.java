b56fa517e28b4cd3866fb03df1f1877eca61e96c
Satish-2005 <satishrathod9322426616@gmail.com>
2025-08-20 18:09:43 +0530

    Add recursive binary search implementation

---
+++ 01-arrays/BinarySearchRecursive.java
@@ -0,0 +1,37 @@
+public class BinarySearchRecursive {
+    public static int binarySearch(int[] numbers, int key) {
+        return binarySearch(numbers, key, 0, numbers.length - 1);
+    }
+
+    private static int binarySearch(int[] numbers, int key, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = start + (end - start) / 2;
+        if (numbers[mid] == key) {
+            return mid;
+        }
+
+        if (numbers[mid] < key) {
+            return binarySearch(numbers, key, mid + 1, end);
+        } else {
+            return binarySearch(numbers, key, start, mid - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 4, 6, 8, 10, 12, 14};
+        int key = 10;
+
+        int index = binarySearch(numbers, key);
+
+        if (index != -1) {
+            System.out.println("Key found at index: " + index);
+        } else {
+            System.out.println("Key not found in the array");
+        }
+    }
+}
+
+
